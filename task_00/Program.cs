a32be9d700fb20c794d782f31927000fa006d303
Eugenia Galkina <yavsehmilee@gmail.com>
2022-04-27 15:57:20 +0300

    1st task folder created

---
+++ task_00/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a==b*b)
+{
+    Console.Write("Первое число является квадратом второго: ");
+}
+else
+{
+    int result = b;
+    Console.Write("Большее число: ");
+    Console.WriteLine (result);
+}
