e631d87f9701a6528e7e5c11ca4a430aec2fdea4
Eugenia Galkina <yavsehmilee@gmail.com>
2022-04-27 18:40:40 +0300

    7th task done

---
+++ task_06/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+int a = n%10;
+int b = (n/10)%10;
+int c = n/100;
+
+if (n>99 && n<1000)
+{
+   
+    Console.Write("число без второй цифры: ");
+    Console.Write(c);
+    Console.Write(a);
+}
+else
+{
+    Console.Write("это не трехзначное число!");
+}
