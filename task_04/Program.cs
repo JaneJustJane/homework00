c7c58add2e3335a54516e91db6fb3a05cd206756
Eugenia Galkina <yavsehmilee@gmail.com>
2022-04-27 17:36:02 +0300

    5th task done

---
+++ task_04/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+if (a>99 && a<1000)
+{
+    a = (a/10)%10;
+    Console.Write("вторая цифра этого числа: ");
+    Console.Write(a);
+}
+
+else
+{
+    Console.Write("число не трехзначное!");
+}
