e4ba003bd7ac1c7d280713dd6c9dc575fe5a2c98
Eugenia Galkina <yavsehmilee@gmail.com>
2022-04-27 18:30:15 +0300

    6th task done

---
+++ task_05/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите число от 10 до 99: ");
+int n = int.Parse(Console.ReadLine());
+int a = n%10;
+int b = (n/10)%10;
+if (n>=10 && n<=99)
+{
+    int max = a;
+    if (b>max) max = b;
+    Console.Write("наибольшая цифра числа: ");
+    Console.Write(max);
+}
+else
+{
+    Console.Write("это число не входит в отрезок от 10 до 99!");
+}
+
