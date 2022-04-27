fab9558d5bc282ce1bd9b12a240c603c3d31d9a8
Eugenia Galkina <yavsehmilee@gmail.com>
2022-04-27 16:51:00 +0300

    3rd task done

---
+++ task_02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a%2;
+if (b==0)
+{
+Console.Write("это число четное!");
+}
+else
+{
+Console.Write("это число нечетное!");
+}
