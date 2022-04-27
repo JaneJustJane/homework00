0ab8a86a52b162dd81d3e7811ed3b5c6a2dde0d9
Eugenia Galkina <yavsehmilee@gmail.com>
2022-04-27 19:44:34 +0300

    9th task done

---
+++ task_08/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a>99)
+{
+   do 
+    { 
+        a=a/10;
+    }   
+    while (a>=999);
+
+    a = a%10;
+    Console.Write("третья цифра числа = ");
+    Console.Write(a);
+}
+else
+Console.WriteLine ("это число двузначное и в нем нет третьей цифры!");
