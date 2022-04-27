6640566474fe14fcfb4ae06dd5493b02a33ae7be
Eugenia Galkina <yavsehmilee@gmail.com>
2022-04-27 18:56:06 +0300

    8th task done

---
+++ task_07/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число, чтобы проверить кратность: ");
+int m = int.Parse(Console.ReadLine());
+int a = n%m;
+if (a==0) 
+{
+Console.WriteLine("первое число кратно второму!");
+}
+else 
+{
+Console.WriteLine("первое число некратно второму!");
+Console.Write("остаток= ");
+Console.Write(a);
+}
